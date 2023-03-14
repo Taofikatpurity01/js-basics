5e4cedcf619b9e85cfa54ba047a138a3b014b013
taofikatpurity01 <taofikatoyedepo@gmail.com>
2023-03-14 15:31:25 +0100

    jsbasic

---
+++ index.js
@@ -0,0 +1,18 @@
+// to multiply two numbers
+const mul = 4 * 8
+console.log(mul)
+
+// sentence
+let firstname = "Taofikat"
+let lastname = "Oyedepo"
+let stuterntrack="frontend development"
+let skill="frontend devlopment"
+let favouritecolour = "green"
+let countryindependence = "1st october 1960"
+console.log( "firstname:"+""+firstname)
+console.log( "lastname:"+""+lastname)
+console.log( "stuterntrack:"+""+stuterntrack)
+
+console.log( "skill:"+""+skill)
+console.log( "favouritecolour:"+""+favouritecolour)
+console.log( "countryindependence:"+""+countryindependence)
